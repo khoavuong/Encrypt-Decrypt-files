4087faabca068dcc98c700cbd2ec6c900554d7b3
khoavuong <50255410+khoavuong@users.noreply.github.com>
2020-05-24 15:01:35 +0700

    clean up comment

--- api/src/file/file.module.ts
+++ api/src/file/file.module.ts
@@ -1,8 +1,7 @@
-import { Module, UnsupportedMediaTypeException } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { MulterModule } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { extname } from 'path';
 import { FileService } from './file.service';
 
 @Module({
@@ -10,16 +9,7 @@ import { FileService } from './file.service';
     MulterModule.register({
       storage: diskStorage({
         destination: './upload',
-        filename: (req, file, callback) => {
-          //const { fieldname, originalname, encoding, mimetype } = file;
-          /* const name = file.originalname.split('.')[0];
-          const fileExtName = extname(file.originalname);
-          const randomName = Array(4)
-            .fill(null)
-            .map(() => Math.round(Math.random() * 16).toString(16))
-            .join(''); */
-          callback(null, file.originalname);
-        },
+        filename: (req, file, callback) => callback(null, file.originalname),
       }),
     }),
   ],
