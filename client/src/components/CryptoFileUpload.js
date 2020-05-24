e7f93b73257fc9d15b45681b91a2a7715b6d1837
khoavuong <50255410+khoavuong@users.noreply.github.com>
2020-05-24 15:00:36 +0700

    Update cipher algorithm & mode options

--- client/src/components/CryptoFileUpload.js
+++ client/src/components/CryptoFileUpload.js
@@ -6,42 +6,73 @@ const { Dragger } = Upload;
 
 export const CryptoFileUpload = ({ type }) => {
   const [fileList, setFileList] = useState([]);
-  const [key, setKey] = useState("");
+  const [password, setPassword] = useState("");
   const [algorithm, setAlgorithm] = useState("");
+  const [mode, setMode] = useState("");
 
   const props = {
     name: "file",
     action: `${process.env.REACT_APP_BASE_URL}/file/${type}`,
-    data: { key, algorithm },
+    data: { password, algorithm, mode },
     onChange(info) {
       const { file } = info;
       if (file.response) {
-        file.url = `${process.env.REACT_APP_BASE_URL}/file/${file.response.filename}`;
-        file.name = file.response.filename;
+        if (file.response.message) file.name = file.response.message;
+        else {
+          file.url = `${process.env.REACT_APP_BASE_URL}/file/${file.response.filename}`;
+          file.name = file.response.filename;
+        }
       }
       setFileList([file]);
     },
+    showUploadList: { showRemoveIcon: false },
   };
 
   return (
     <div>
-      <p style={{ fontWeight: "bold" }}>Choose an algorithm:</p>
+      <p style={{ fontWeight: "bold" }}>Choose a cipher algorithm:</p>
       <Select
         style={{ width: "100%" }}
-        placeholder="Chose an algorithm"
-        onChange={(value) => setAlgorithm(value)}
+        placeholder="Choose your algorithm"
+        onChange={(value) => {
+          setAlgorithm(value);
+          setMode("");
+        }}
       >
-        <Select.Option value="aes-256-ctr">aes-256-ctr</Select.Option>
+        <Select.Option value="aes-128">AES-128</Select.Option>
+        <Select.Option value="aes-192">AES-192</Select.Option>
+        <Select.Option value="aes-256">AES-256</Select.Option>
+        <Select.Option value="des">DES</Select.Option>
+        <Select.Option value="des-ede">3DES-2-keys</Select.Option>
+        <Select.Option value="des-ede3">3DES-3-keys</Select.Option>
       </Select>
 
       <br />
       <br />
 
-      <p style={{ fontWeight: "bold" }}>Enter a key:</p>
+      <p style={{ fontWeight: "bold" }}>Choose a cipher mode:</p>
+      <Select
+        style={{ width: "100%" }}
+        onChange={(value) => setMode(value)}
+        value={mode}
+      >
+        {algorithm.slice(4, 7) !== "ede" ? (
+          <Select.Option value="ecb">ECB</Select.Option>
+        ) : null}
+
+        <Select.Option value="cbc">CBC</Select.Option>
+        <Select.Option value="cfb">CFB</Select.Option>
+        <Select.Option value="ofb">OFB</Select.Option>
+      </Select>
+
+      <br />
+      <br />
+
+      <p style={{ fontWeight: "bold" }}>Enter a password:</p>
       <Input
-        placeholder="Enter your key here"
-        value={key}
-        onChange={(e) => setKey(e.target.value)}
+        placeholder="Enter your password here"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
 
       <br />
